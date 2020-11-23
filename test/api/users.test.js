68fe3fcb90a88a981dd2778364359bec96fc9dcb
Julian <jfischetti94@gmail.com>
2020-11-23 03:40:14 -0300

    Email not registered validation

--- test/api/users.test.js
+++ test/api/users.test.js
@@ -24,4 +24,22 @@ describe('Create', () => {
     expect(res.body).toBeObject();
     expect(res.body).toContainEntry(['created', true]);
   });
+
+  test('should responde 400 when try to register an email two times', async () => {
+    const user = {
+      email: 'jon.snow@mail.com',
+      password: 'nightWatch',
+    };
+    let res = await request(app).post('/users').send(user);
+    expect(res.status).toBe(CREATED);
+
+    res = await request(app).post('/users').send(user);
+    expect(res.status).toBe(BAD_REQUEST);
+    expect(res.body).toBeObject();
+    expect(res.body).toContainEntry(['status', BAD_REQUEST]);
+    expect(res.body).toContainEntry(['message', 'User not created']);
+    expect(res.body.errors).toIncludeAllMembers([
+      'E-Mail address already exists',
+    ]);
+  });
 });
